aea5634332656b55764cbd0b343a9c9aa01b1846
Viviana Molina <viviana.a.molina@gmail.com>
2025-03-18 12:09:09 -0700

    Added logic to start assigning the secret friend

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombre = inputAmigo.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+
+    inputAmigo.value = "";
+
+    actualizarLista();
+}
+
+function actualizarLista() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    amigos.forEach((amigo) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    let resultado = document.getElementById("resultado");
+
+    if (amigos.length === 0) {
+        resultado.innerHTML = "<p style='color: red;'>No hay amigos en la lista para sortear.</p>";
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    resultado.innerHTML = `<p>El amigo secreto es: <strong>${amigoSorteado}</strong></p>`;
+}
+
+function Reiniciar(set){
+    
+    let divReiniciar = document.getElementById('reiniciar');
+    
+    if(set =='si'){
+        divReiniciar.hidden = false;
+    }
+    else
+    {
+        divReiniciar.hidden = true;
+    }
+}
+
+function limparArray(){
+    amigos = [];
+    numerosGenerados = [];
+    let MiLista = document.getElementById('listaAmigos');
+    MiLista.innerHTML = '';
+    let sorteado = document.getElementById('resultado');
+    sorteado.innerHTML = '';
+    Reiniciar('no');
+}
